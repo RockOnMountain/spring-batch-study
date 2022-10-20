25401676181d7af0a96926676b89d8eacb7d7ec9
RockOnMountain <matht1115@naver.com>
2022-10-20 20:23:42 +0900

    feat: 배치 프로그램 돌아가게 수정

--- src/main/java/com/study/springbatch/dbSchema/DBJobConfig.java
+++ src/main/java/com/study/springbatch/dbSchema/DBJobConfig.java
@@ -3,11 +3,8 @@ package com.study.springbatch.dbSchema;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,12 +16,10 @@ public class DBJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
+
     @Bean
     public Job job() {
-        return jobBuilderFactory.get("job")
-                .start(step1())
-                .next(step2())
-                .build();
+        return jobBuilderFactory.get("job").start(step1()).next(step2()).build();
     }
 
 
