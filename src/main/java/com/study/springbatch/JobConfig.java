7f31d783b625d791fb2cd071ab4a7b761ced160f
RockOnMountain <matht1115@naver.com>
2022-10-26 08:27:29 +0900

    feat: JobParamters study

--- src/main/java/com/study/springbatch/JobConfig.java
+++ src/main/java/com/study/springbatch/JobConfig.java
@@ -1,7 +1,9 @@
 package com.study.springbatch;
 
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -19,21 +21,28 @@ public class JobConfig {
 
     @Bean
     public Job job() {
-        return jobBuilderFactory.get("job")
-                .start(step1())
-                .next(step2())
-                .build();
+        return jobBuilderFactory.get("job").start(step1()).next(step2()).build();
     }
 
 
     @Bean
     public Step step1() {
-        return stepBuilderFactory.get("step1")
-                .tasklet((contribution, chunkContext) -> {
+        return stepBuilderFactory.get("step1").tasklet((contribution, chunkContext) -> {
+
+            // 디버그 모드로 확인해 볼 것
+            JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
+            jobParameters.getString("name");
+            jobParameters.getLong("seq");
+            jobParameters.getDate("date");
+            jobParameters.getDouble("age");
+
+            // Map<String, Object> jobParametersMap = chunkContext.getStepContext().getJobParameters();
+
             System.out.println("step1 was executed");
             return RepeatStatus.FINISHED;
         }).build();
     }
+
 
     @Bean
     public Step step2() {
--- src/main/java/com/study/springbatch/JobRunner.java
+++ src/main/java/com/study/springbatch/JobRunner.java
@@ -1,5 +1,6 @@
 package com.study.springbatch;
 
+import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
@@ -16,10 +17,20 @@ public class JobRunner implements ApplicationRunner {
     private final JobLauncher jobLauncher;
     private final Job job;
 
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        JobParameters jobParameters = new JobParametersBuilder().addString("name", "user2").toJobParameters();
+        /*
+            JobParameters 와 JobInstance 는 1:1 관계
+            JobParameters 는 Map 을 감싼 일급컬렉션 이다.
+         */
+
+        // JobParameters jobParameters = new JobParametersBuilder().addString("name", "user2").toJobParameters();
+        JobParameters jobParameters =
+                new JobParametersBuilder().addString("name", "user1").addLong("seq", 2L).addDate("date", new Date())
+                        .addDouble("age", 16.5).toJobParameters();
+
         jobLauncher.run(job, jobParameters);
     }
 }
