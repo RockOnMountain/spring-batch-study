a64e99e64a97add62b398ebd3ba78343046f9644
RockOnMountain <matht1115@naver.com>
2022-10-26 08:59:34 +0900

    feat: JobExecution study

--- src/main/java/com/study/springbatch/JobConfig.java
+++ src/main/java/com/study/springbatch/JobConfig.java
@@ -5,8 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,14 +31,7 @@ public class JobConfig {
     public Step step1() {
         return stepBuilderFactory.get("step1").tasklet((contribution, chunkContext) -> {
 
-            // 디버그 모드로 확인해 볼 것
-            JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
-            jobParameters.getString("name");
-            jobParameters.getLong("seq");
-            jobParameters.getDate("date");
-            jobParameters.getDouble("age");
-
-            // Map<String, Object> jobParametersMap = chunkContext.getStepContext().getJobParameters();
+            this.printJobParameters(contribution, chunkContext);
 
             System.out.println("step1 was executed");
             return RepeatStatus.FINISHED;
@@ -44,10 +39,26 @@ public class JobConfig {
     }
 
 
+    private void printJobParameters(StepContribution contribution, ChunkContext chunkContext) {
+
+        // 디버그 모드로 확인해 볼 것
+        JobParameters jobParameters = contribution.getStepExecution().getJobExecution().getJobParameters();
+        jobParameters.getString("name");
+        jobParameters.getLong("seq");
+        jobParameters.getDate("date");
+        jobParameters.getDouble("age");
+
+        // JobParameters를 확인할 수 있는 다른 방법 -> 위의 방법을 추천
+        Map<String, Object> jobParametersMap = chunkContext.getStepContext().getJobParameters();
+    }
+
+
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step1").tasklet((contribution, chunkContext) -> {
+
             System.out.println("step2 was executed");
+            // throw new RuntimeException();
             return RepeatStatus.FINISHED;
         }).build();
     }
--- src/main/java/com/study/springbatch/JobRunner.java
+++ src/main/java/com/study/springbatch/JobRunner.java
@@ -26,10 +26,27 @@ public class JobRunner implements ApplicationRunner {
             JobParameters 는 Map 을 감싼 일급컬렉션 이다.
          */
 
-        // JobParameters jobParameters = new JobParametersBuilder().addString("name", "user2").toJobParameters();
+        /*
+            JobInstance JobExecution JobExecutionParam 관계들
+            1) JobParameters 와 JobInstance 는 1:1 관계가 맞다.
+            2) JobInstance 와 JobExecution 은 1:N 관계이다.
+            3) JobExecution 과 JobExecutionParam 은 1:1 관계이다.
+            4) 주의해야할 점은 JobParameters 와 JobExecutionParam 은 다르다는 것이다.
+
+            JobInstance 가 Completed 인 경우는 JobExecution 이 더 이상 생기지 않는다.
+            그러나 JobInstance 가 Failed 인 경우는 JobExecution 이 더 생긴다.
+            참고) https://backtony.github.io/assets/img/post/spring/batch/2/2-6.PNG
+         */
+
+        /*
+            JobParameters jobParameters =
+                new JobParametersBuilder().addString("name", "user1").addLong("seq", 3L).addDouble("age", 16.7)
+                        .addDate("date", new Date()).toJobParameters();
+         */
+
         JobParameters jobParameters =
-                new JobParametersBuilder().addString("name", "user1").addLong("seq", 2L).addDate("date", new Date())
-                        .addDouble("age", 16.5).toJobParameters();
+                new JobParametersBuilder().addLong("seq", 2L)
+                        .toJobParameters();
 
         jobLauncher.run(job, jobParameters);
     }
