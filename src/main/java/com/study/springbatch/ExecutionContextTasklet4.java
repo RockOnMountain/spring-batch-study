44b2876064cecf68df09dcf02c61978a6f5dd290
RockOnMountain <matht1115@naver.com>
2022-10-28 09:12:03 +0900

    feat: ExecutionContext study

--- src/main/java/com/study/springbatch/ExecutionContextTasklet4.java
+++ src/main/java/com/study/springbatch/ExecutionContextTasklet4.java
@@ -3,14 +3,21 @@ package com.study.springbatch;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
 
-public class CustomTasklet implements Tasklet {
+@Component
+public class ExecutionContextTasklet4 implements Tasklet {
 
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 
-        System.out.println("step1 was executed");
+        System.out.println("step4 was executed");
+
+        ExecutionContext jobExecutionContext = contribution.getStepExecution().getJobExecution().getExecutionContext();
+        System.out.println("name: " + jobExecutionContext.get("name"));
+
         return RepeatStatus.FINISHED;
     }
 }
