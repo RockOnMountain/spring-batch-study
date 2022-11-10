ec6b8c1b688e22e1589e5f7846f07623c59cd2c8
RockOnMountain <matht1115@naver.com>
2022-11-10 09:52:02 +0900

    feat: study incrementer

--- src/main/java/com/study/springbatch/SimpleJobConfig.java
+++ src/main/java/com/study/springbatch/SimpleJobConfig.java
@@ -21,22 +21,11 @@ public class SimpleJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-
     /*
-        @Autowired(required = false) : 자동 주입할 대상이 없으면 수정자 메서드 자체가 호출 안됨
-     */
+        incrementer
 
-    /*
-        JobBuilderFactory 아키텍처
-        https://s3.us-west-2.amazonaws.com/secure.notion-static.com/1617f82d-8e64-4a48-acf5-8f16385d4589/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221102%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221102T233713Z&X-Amz-Expires=86400&X-Amz-Signature=d168b0731888dc0fb264701225396142ac8b86e5b473ab84cee73fa46b7a0fb9&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject
-
-        JobBuilderFactory 상속구조
-        https://s3.us-west-2.amazonaws.com/secure.notion-static.com/003fdfc6-724a-434b-8ae9-bbdb8fb588e3/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221102%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221102T233847Z&X-Amz-Expires=86400&X-Amz-Signature=08be826a13565b850ff6ecc7f75a554724bf54ab3a4622717f9cb77054d93dec&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject
-     */
-
-    /*
-        SimpleJob 은 JobBuilderHelper 를 상속받는다.
-        JobBuilderHelper 에는 CommonJobProperties 가 있는데 여기에 listener, validator 등에 대한 정보가 담긴다.
+        기존의 JobParameters 변경없이 Job 을 여러번 실행하고자 할 때 사용
+        내가 설정한 parameters 외에 id 라는 파라미터를 설정 후 계속 증가시키므로 같은 Job 이라도 계속 실행시킬 수 있다.
      */
     @Bean
     public Job job() {
@@ -44,20 +33,6 @@ public class SimpleJobConfig {
                 .start(step1())
                 .next(step2())
                 .incrementer(new RunIdIncrementer())
-                .validator(parameters -> {})
-                .listener(new JobExecutionListener() {
-
-                    @Override
-                    public void beforeJob(JobExecution jobExecution) {
-
-                    }
-
-
-                    @Override
-                    public void afterJob(JobExecution jobExecution) {
-
-                    }
-                })
                 .build();
     }
 
@@ -74,8 +49,6 @@ public class SimpleJobConfig {
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2").tasklet((contribution, chunkContext) -> {
-            chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
-            contribution.setExitStatus(ExitStatus.STOPPED);
             System.out.println("step2 executed");
             return RepeatStatus.FINISHED;
         }).build();
